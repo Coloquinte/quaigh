42510cb07c0b5253688c6a0e5643b0841ae81e76
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-12-01 17:50:51 +0000

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-//! Implementation of And-Inverter-Graph for logic simplification (AIG).
+//! Logic simplification and analysis tools
 //!
-//! This crate provides a flexible representation for logic graphs based on a network of logic gates with implicit inverters.
-//! It provides utilities to manipulate and simplify logic functions, check for equivalence, ...
-//!
-//! It is inspired by the logic synthesis tools [ABC](https://people.eecs.berkeley.edu/~alanmi/abc/) and [Mockturtle](https://mockturtle.readthedocs.io/en/latest/).
-//! Our goal is to provide an easy-to-use library for logical synthesis and technology mapping, and improve its quality over time to match industrial tools.
+//! This crate provides tools for logic optimization, synthesis, technology mapping and analysis.
+//! Our goal is to provide an easy-to-use library, and improve its quality over time to match industrial tools.
 //!
 //! # Design
 //!
 //! Quaigh main representation uses a typical Gate-Inverter-Graph to represent a logic circuit.
+//! Gates are represented explicitly, and inverters are implicit, occupying just one bit.
 //!
 //! To make optimization easier, it differs from most similar representations:
 //! * Complex gates such as Xor, Mux and Maj3 are all first class citizens and can coexist in the same logic circuit;
@@ -16,9 +14,9 @@
 //!
 //! # Features
 //!
-//! Quaigh features bounded equivalence checking, AIG simplification and basic ATPG.
-//! At the moment, these are far from state of the art: for production designs, please use ABC for logic simplification
-//! and Atalanta for test pattern generation.
+//! Quaigh features bounded equivalence checking, logic simplification and basic ATPG.
+//! At the moment, these are far from state of the art: for production designs, you should generally
+//! stick to the tools included in [Yosys](https://github.com/YosysHQ/yosys).
 //!
 //! # Usage
 //!
