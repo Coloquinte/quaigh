4ba37bee9c6ed9a633d7912f2fc5640672e0435f
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-12-20 15:36:05 +0000

    Do not use the barebones equivalence checking in tests anymore (too slow) + more info on optimization results

--- scripts/opt_benchmarks.sh
+++ scripts/opt_benchmarks.sh
@@ -9,22 +9,16 @@ do
 	mkdir -p "${dir}"
 done
 
-for benchmark in bench/c*.bench
+for benchmark in bench/*.bench
 do
         name=$(basename "${benchmark}" .bench)
-	echo "Running benchmark ${name} with safe check"
+	echo "Running benchmark ${name}"
 	output_file="opt/${name}.bench"
 	quaigh opt "${benchmark}" -o "${output_file}" --seed 1 || { echo "Optimization failure on ${name}"; exit 1; }
-	echo -ne '\tOptimization done\n\t'
-	quaigh equiv "${benchmark}" "${output_file}" -c 5 --sat-only || { echo "Equivalence failure on ${name}"; exit 1; }
-done
-
-for benchmark in bench/b*.bench bench/s*.bench
-do
-        name=$(basename "${benchmark}" .bench)
-	echo "Running benchmark ${name} with fast check"
-	output_file="opt/${name}.bench"
-	quaigh opt "${benchmark}" -o "${output_file}" --seed 1 || { echo "Optimization failure on ${name}"; exit 1; }
+	echo "Initial stats:"
+	quaigh show "${benchmark}"
+	echo "Final stats:"
+	quaigh show "${output_file}"
 	echo -ne '\tOptimization done\n\t'
 	quaigh equiv "${benchmark}" "${output_file}" -c 5 || { echo "Equivalence failure on ${name}"; exit 1; }
 done
